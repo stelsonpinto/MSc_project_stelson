f144a922a55e14aae905eb63784c7be118533f5d
stelsonpinto <k25106429@kcl.ac.uk>
2026-05-16 17:41:21 +0100

    06_create_ld_reference.sh

--- CODE/06_create_ld_reference.sh
+++ CODE/06_create_ld_reference.sh
@@ -5,6 +5,7 @@
 #SBATCH --time=12:00:00
 #SBATCH --output=logs/liftover_%j.out
 
+mkdir -p chr_data
 module load plink2
 
 plink2 --pfile all_hg38 vzs --max-alleles 2 --remove remove_related_fixed.txt --keep filtered_samples_EUR_fixed.txt --allow-extra-chr --make-pgen vzs --out all_hg38_EUR
@@ -14,4 +15,16 @@ plink2 --pfile all_hg38_EUR vzs --ref-from-fa GRCh38_full_analysis_set_plus_deco
 plink2 --pfile all_hg38_EUR_ref vzs --rm-dup force-first --make-pgen vzs --allow-extra-chr --out all_hg38_EUR_ref_nodup
 
 plink2 --pfile all_hg38_EUR_ref_nodup vzs --allow-extra-chr --make-just-pvar --out all_hg38_EUR_ref_nodup_unzipped
+
+# create chromosome specic datasets
+awk 'NR>1 {print $2}' ST3_with_gene_names.tsv | sort -u | while read chr; do
+
+  plink2 \
+    --pfile all_hg38_EUR_ref_nodup vzs \
+    --chr ${chr} \
+    --allow-extra-chr \
+    --make-pgen vzs \
+    --out chr_data/all_hg38_chr${chr}_EUR_ref_nodup
+
+done
 
